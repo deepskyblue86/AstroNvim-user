148b436d0ab2bb682bff5f9b032576cfa028b30f
Angelo Puglisi <angelo.puglisi@sysdig.com>
2023-04-07 20:36:24 +0200

    feat: add git-blame and trouble
    Mind lazy false, otherwise they won't start

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,20 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "f-person/git-blame.nvim",
+    config = function()
+      vim.g.gitblame_date_format = "%r"
+      vim.g.gitblame_message_template = "		// <author>, <date> • <summary> • <sha>"
+    end,
+    lazy = false,
+  },
+  {
+    "folke/trouble.nvim",
+    event = "BufReadPre",
+    wants = "nvim-web-devicons",
+    cmd = { "TroubleToggle", "Trouble" },
+    config = function() require("trouble").setup { use_diagnostic_signs = true } end,
+    lazy = false,
+  },
 }
